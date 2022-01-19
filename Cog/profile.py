80ea00af966c8685d4d702bf189b3de07f6781d7
AS1 <arsenikofanv@gmail.com>
2022-01-19 19:17:54 +0300

    -

--- Cog/profile.py
+++ Cog/profile.py
@@ -251,11 +251,11 @@ class profile(commands.Cog):
 
         idraw.text((60,195), f"{text} #{topvoice}" , font = para)
 
-        if user['guild'] != None:
-            club = server['rpg']['guild'][f'{user["guild"]}']
-            idraw.text((15,360), f"{name}#{tag} [{club['tag']}]", font = headline) #первое значение это отступ с лева, второе сверху
-        else:
-            idraw.text((15,360), f"{name}#{tag}", font = headline)
+        # if user['guild'] != None:
+        #     club = server['rpg']['guild'][f'{user["guild"]}']
+        #     idraw.text((15,360), f"{name}#{tag} [{club['tag']}]", font = headline) #первое значение это отступ с лева, второе сверху
+        # else:
+        #     idraw.text((15,360), f"{name}#{tag}", font = headline)
 
         idraw.text((60,242), f"{user['money']} #{topmn}", font = para)
         idraw.text((260,50), f"{len(user['rep'][0])}", font = para)
